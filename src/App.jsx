db743caad473ccca7fbfae9d13cccccd62b9a7e7
CaptainAwesomeRaj <raj6264186210@gmail.com>
2023-10-12 18:15:47 +0530

    installed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+
 function App() {
   return (
-    <>lol</>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,7 @@
 function Register() {
     return ( 
         <>
+        Register
         </>
     );
 }
