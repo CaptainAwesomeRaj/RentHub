2175dd20038a3622d86c1af924ee4e2262fe4e5a
CaptainAwesomeRaj <raj6264186210@gmail.com>
2023-10-16 13:49:36 +0530

    nothing special

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,9 @@ import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      App
+    </>
 
   );
 }
